64cbd62e8b66ad6506151423fe88fb9a869190a1
Melissa Garza <melissa@zeitcode.com>
2014-02-28 14:08:37 -0600

    updated to ckeditor 4.3.3

--- tests/manage.py
+++ tests/manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
+    sys.path.insert(0, "/Users/melissa/Zeitcode/projects/django-ckeditor")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testsite.settings")
 
     from django.core.management import execute_from_command_line
