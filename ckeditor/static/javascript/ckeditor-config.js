5df56af004f65c1cb52fcb280b79741d0dae9591
Melissa Garza <melissa@zeitcode.com>
2014-03-05 15:04:19 -0600

    applied old configuration to the new config format

--- ckeditor/static/javascript/ckeditor-config.js
+++ ckeditor/static/javascript/ckeditor-config.js
@@ -1,16 +1,18 @@
 CKEDITOR.editorConfig = function( config ) {
     config.toolbar = [
-			['Format', 'Bold', 'Italic', 'Underline', 'Strikethrough', '-', 'Subscript', 'Superscript', '-', 'TextColor'],
-			['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
-            ['NumberedList', 'BulletedList', '-', 'Blockquote'],
-            ['Link', 'Unlink'],
-            ['Table', '-', 'Image'],
-            ['ShowBlocks', 'Source']
-		];
-
-	config.removeButtons = 'Underline,Subscript,Superscript';
-
-	config.format_tags = 'p;h1;h2;h3;pre';
-
-	config.removeDialogTabs = 'image:advanced;link:advanced';
+        ['Format', 'Bold', 'Italic', 'Underline', 'Strikethrough', '-', 'Subscript', 'Superscript', '-', 'TextColor'],
+        ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+        ['NumberedList', 'BulletedList', '-', 'Blockquote'],
+        ['Link', 'Unlink'],
+        ['Table', '-', 'Image'],
+        ['ShowBlocks', 'Source']
+    ];
+    config.toolbarCanCollapse = true;
+	config.format_tags = 'p;h2;h3;h4;h5';
+    config.coreStyles_strike = {element: 'del', overrides: 'strike'};
+    config.coreStyles_underline = {element: 'ins', overrides: 'u'};
+    config.scayt_autoStartup = false;
+    config.disableNativeSpellChecker = false;
+    config.filebrowserUploadUrl = '/ckeditor/upload/';
+    config.filebrowserBrowseUrl = '/ckeditor/browse/';
 };
