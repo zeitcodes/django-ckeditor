d2dceb6e6ef80e63117a5cd5d687182f3b36d274
Melissa Garza <melissa@zeitcode.com>
2014-03-05 15:35:56 -0600

    restructured custom config file

--- ckeditor/static/javascript/ckeditor-config.js
+++ ckeditor/static/javascript/ckeditor-config.js
@@ -1,18 +1,24 @@
 CKEDITOR.editorConfig = function( config ) {
-    config.toolbar = [
-        ['Format', 'Bold', 'Italic', 'Underline', 'Strikethrough', '-', 'Subscript', 'Superscript', '-', 'TextColor'],
-        ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
-        ['NumberedList', 'BulletedList', '-', 'Blockquote'],
-        ['Link', 'Unlink'],
-        ['Table', '-', 'Image'],
-        ['ShowBlocks', 'Source']
-    ];
-    config.toolbarCanCollapse = true;
-	config.format_tags = 'p;h2;h3;h4;h5';
-    config.coreStyles_strike = {element: 'del', overrides: 'strike'};
-    config.coreStyles_underline = {element: 'ins', overrides: 'u'};
-    config.scayt_autoStartup = false;
-    config.disableNativeSpellChecker = false;
-    config.filebrowserUploadUrl = '/ckeditor/upload/';
-    config.filebrowserBrowseUrl = '/ckeditor/browse/';
+
+    var options = {
+        toolbar: [
+            ['Format', 'Bold', 'Italic', 'Underline', 'Strikethrough', '-', 'Subscript', 'Superscript', '-', 'TextColor'],
+            ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            ['NumberedList', 'BulletedList', '-', 'Blockquote'],
+            ['Link', 'Unlink'],
+            ['Table', '-', 'Image'],
+            ['ShowBlocks', 'Source']
+        ],
+        toolbarCanCollapse: true,
+        format_tags: 'p;h2;h3;h4;h5',
+        coreStyles_strike: {element: 'del', overrides: 'strike'},
+        coreStyles_underline: {element: 'ins', overrides: 'u'},
+        scayt_autoStartup: false,
+        disableNativeSpellChecker: false,
+        filebrowserUploadUrl: '/ckeditor/upload/',
+        filebrowserBrowseUrl: '/ckeditor/browse/'
+    };
+
+    $.extend(config, options);
+
 };
