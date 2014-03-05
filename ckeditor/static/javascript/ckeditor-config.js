c87687444fcd1598c9ecb3e93b00bb21dc1ac1c9
Melissa Garza <melissa@zeitcode.com>
2014-03-05 14:25:08 -0600

    removed standard ckeditor

--- ckeditor/static/javascript/ckeditor-config.js
+++ ckeditor/static/javascript/ckeditor-config.js
@@ -1,20 +1,12 @@
 CKEDITOR.editorConfig = function( config ) {
-	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
-	];
+    config.toolbar = [
+			['Format', 'Bold', 'Italic', 'Underline', 'Strikethrough', '-', 'Subscript', 'Superscript', '-', 'TextColor'],
+			['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            ['NumberedList', 'BulletedList', '-', 'Blockquote'],
+            ['Link', 'Unlink'],
+            ['Table', '-', 'Image'],
+            ['ShowBlocks', 'Source']
+		];
 
 	config.removeButtons = 'Underline,Subscript,Superscript';
 
